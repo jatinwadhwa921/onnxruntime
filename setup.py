10b80a57d2148588e3dcb50a7bd6d0920213c96c
jatinwadhwa921 <jatin.wadhwa@intel.com>
2025-02-14 17:03:55 +0530

    Fix for manylinux wheels 1_20

--- setup.py
+++ setup.py
@@ -416,6 +416,7 @@ if is_manylinux:
         ov_libs = [
             "libopenvino_intel_cpu_plugin.so",
             "libopenvino_intel_gpu_plugin.so",
+            "libopenvino_intel_npu_plugin.so"
             "libopenvino_auto_plugin.so",
             "libopenvino_hetero_plugin.so",
             "libtbb.so.2",
@@ -423,6 +424,10 @@ if is_manylinux:
             "libopenvino.so",
             "libopenvino_c.so",
             "libopenvino_onnx_frontend.so",
+            "libopenvino.so.2500",
+            "libopenvino_c.so.2500",
+            "libopenvino_onnx_frontend.so.2500",
+            "libtbb.so.12"
         ]
         for x in ov_libs:
             y = "onnxruntime/capi/" + x
